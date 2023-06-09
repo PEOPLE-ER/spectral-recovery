6e128b524d5fb6f4eb9f5a6aba2107355d78ca60
szwiep <sarahzwiep@gmail.com>
2023-06-09 15:22:21 -0700

    Add skeleton of classes related to restoration area methods

--- spectral-recovery/spectral_recovery.py
+++ spectral-recovery/spectral_recovery.py
@@ -1,65 +1,72 @@
 import rioxarray
 import dask
-
+import os
+os.environ['USE_PYGEOS'] = '0'
 import xarray as xr
 import geopandas as gpd
 import numpy as np
 import pandas as pd
 
-from shapely.geometry import Polygon
-from typing import Optional, Union, List, Dict
+from geopandas import GeoDataFrame
+from typing import Optional, Union, List
 
-from restoration_area import RestorationArea
+from reference import RestorationArea
+from spectralstack import MultiYearlyStack
 
 
 
 def spectral_recovery(
-        restoration_poly: Union[str, Polygon],
-        restoration_year: int, 
-) -> None:
+        restoration_poly: Union[str, gpd.GeoDataFrame],
+        restoration_year: int,
+        reference_range: Union[int, List[int]]
+        ) -> None:
         """
         The main calling function. Better doc-string incoming. 
 
         Parameters
         -----------
-        restoration_poly_path : str
-            Path to vector file containing polygon representing restoration area.
+        restoration_poly : str or Polygon
+            Path to vector file or Polygon object with polygon
+            representing restoration area
 
         restoration_year :
             Year of restoration event.
 
         """
-        if isinstance(restoration_area, str):
-            restoration_vectors = gpd.read_file(restoration_poly)
-            if restoration_vectors.shape[0] != 1:
-                return ValueError("Only one restoration polygon can be provided.")
+        # Read file if given filename
+        if isinstance(restoration_poly, str):
+            restoration_poly = gpd.read_file(restoration_poly)
+            # TODO: check if individual polygon given? even though it's checked 
+            # at initializiation of RestorationArea instance?      
+        restoration_area = RestorationArea(
+                 restoration_polygons=restoration_poly,
+                 restoration_year=restoration_year,
+                 reference_range=reference_range
+                )
         
-            restoration_poly = restoration_vectors.iloc[0].geom
-            restoration_area = RestorationArea(restoration_poly, restoration_year)
-        else:
-            restoration_area = restoration_poly
-
-
-        np.random.seed(0)
-        temperature = 15 + 8 * np.random.randn(3, 2, 2)
-        lon = [[-99.83, -99.32], [-99.79, -99.23]]
-        lat = [[42.25, 42.21], [42.63, 42.59]]
-        time = pd.date_range("2014-09-06", periods=3)
-        reference_time = pd.Timestamp("2014-09-05")
-        test_stack = xr.DataArray(
-        data=temperature,
-        dims=["time", "y", "x"],
-        coords=dict(
-                lon=(["y", "x"], lat),
-                lat=(["y", "x"], lon),
-                time=time,
-                reference_time=reference_time,
-        ),
-        attrs=dict(
-                description="Ambient temperature.",
-                units="degC",
-        ),
-        )
-        restoration_area.clip_from_stack(test_stack)
-
+        test_stack = rioxarray.open_rasterio("../data/nir_18_19.tif",
+                                             chunks="auto")
+        print(test_stack)
+        stack = MultiYearlyStack({"b_one": test_stack, "b_two": test_stack})
+        if not stack.contains(restoration_area):
+            return ValueError(
+                f"Restoration polygon is not within temporal or spatial bounds "
+                f"of the provided timeseries."
+                )
+        clipped_stack = stack.clip(restoration_area)
+        # for index in indices:
+        #     index_array = getattr(spectral_recovery, )
         return
+
+def compute_metrics():
+      pass
+
+def compute_index():
+      pass
+
+
+if __name__ == "__main__":
+      test_poly = gpd.read_file("../data/test_poly.gpkg")
+      rest_year = 2019
+      reference_year = 2018 
+      spectral_recovery(test_poly, rest_year, reference_year)
