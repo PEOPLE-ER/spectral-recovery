500599bbb68a9bcb50f364659cd19578e903c9fc
szwiep <sarahzwiep@gmail.com>
2023-06-12 12:25:28 -0700

    Implement contains feature for spatial and temporal timeseries validation

--- spectral-recovery/spectral_recovery.py
+++ spectral-recovery/spectral_recovery.py
@@ -8,14 +8,15 @@ import numpy as np
 import pandas as pd
 
 from geopandas import GeoDataFrame
-from typing import Optional, Union, List
+from typing import Optional, Union, List, Dict
 
 from reference import RestorationArea
-from spectralstack import MultiYearlyStack
+from spectralstack import MultiBandYearlyStack
 
 
 
 def spectral_recovery(
+        band_dict: Dict[str, xr.DataArray],
         restoration_poly: Union[str, gpd.GeoDataFrame],
         restoration_year: int,
         reference_range: Union[int, List[int]]
@@ -37,23 +38,20 @@ def spectral_recovery(
         if isinstance(restoration_poly, str):
             restoration_poly = gpd.read_file(restoration_poly)
             # TODO: check if individual polygon given? even though it's checked 
-            # at initializiation of RestorationArea instance?      
+            # at initializiation of RestorationArea instance? 
         restoration_area = RestorationArea(
                  restoration_polygons=restoration_poly,
                  restoration_year=restoration_year,
                  reference_range=reference_range
-                )
-        
-        test_stack = rioxarray.open_rasterio("../data/nir_18_19.tif",
-                                             chunks="auto")
-        print(test_stack)
-        stack = MultiYearlyStack({"b_one": test_stack, "b_two": test_stack})
+        )
+        stack = MultiBandYearlyStack(band_dict)
         if not stack.contains(restoration_area):
-            return ValueError(
+            print(
                 f"Restoration polygon is not within temporal or spatial bounds "
-                f"of the provided timeseries."
+                f"of the provided timeseries. Exiting..."
                 )
-        clipped_stack = stack.clip(restoration_area)
+            return
+        # clipped_stack = stack.clip(restoration_area)
         # for index in indices:
         #     index_array = getattr(spectral_recovery, )
         return
@@ -66,7 +64,15 @@ def compute_index():
 
 
 if __name__ == "__main__":
-      test_poly = gpd.read_file("../data/test_poly.gpkg")
-      rest_year = 2019
-      reference_year = 2018 
-      spectral_recovery(test_poly, rest_year, reference_year)
+    test_poly = gpd.read_file("../data/test_poly.gpkg")
+    bad_poly = gpd.read_file("../data/out_of_bounds_poly.gpkg")
+    rest_year = 2019
+    reference_year = [2018, 2020]
+
+    test_stack = rioxarray.open_rasterio("../data/nir_18_19.tif",
+                                             chunks="auto")
+    test_stack = test_stack.rename({"band":"time"})
+    test_stack = test_stack.assign_coords(time=([2018]))
+    test_stack2 = test_stack.assign_coords(time=([2019]))
+    test_band_dict = {"b_one": test_stack, "b_two": test_stack2}
+    spectral_recovery(test_band_dict, test_poly, rest_year, reference_year)
--- spectral-recovery/spectralstack.py
+++ spectral-recovery/spectralstack.py
@@ -1,4 +1,4 @@
-from typing import Dict, Type
+from typing import Dict, Type, Union, List
 from baseline import historic_average
 
 import xarray as xr
@@ -6,24 +6,55 @@ import geopandas as gpd
 import pandas as pd
 
 from reference import ReferenceSystem, RestorationArea
+from shapely.geometry import box
 
-class MultiYearlyStack():
+class MultiBandYearlyStack():
 
     def __init__(
             self, 
             bands_dict: Dict[str, xr.DataArray], 
-            data_mask: xr.DataArray = None,
-    ) -> None:
-        stacked_stack = self._stack_bands(bands_dict)
-        self.stack = stacked_stack
+            data_mask: xr.DataArray = None
+            ) -> None:
+        # validate dimension names
+        self.stack = self._stack_bands(bands_dict)
         self.data_mask = data_mask
 
-    def contains(self, polygons: Type[ReferenceSystem]):
+    def contains(self, reference_system: Type[ReferenceSystem]):
+        # NOTE: if this changes to looking at individual polygons
+        # rather than the bbox of all polygons, consider this algo:
+        # https://stackoverflow.com/questions/14697442/faster-way-of-polygon-intersection-with-shapely
+        # otherwise,
+        contains_flag = True
+        if not self._contains_spatial(reference_system.polygons):
+            contains_flag = False
+        if not self._contains_temporal(reference_system.reference_range):
+            contains_flag = False
+        if isinstance(reference_system, RestorationArea):
+            if not self._contains_temporal(reference_system.restoration_year):
+                contains_flag = False
+        return contains_flag
 
-        pass
+    def _contains_spatial(self, polygons: gpd.GeoDataFrame):
+        ext = box(*self.stack.rio.bounds())
+        poly_ext = box(*polygons.total_bounds)
+        if not ext.contains(poly_ext):
+            return False
+        return True
+        
+    def _contains_temporal(self, years: Union[int, List[int]]):
+        if isinstance(years, list) and len(years) > 1:
+            required_years = range(years[0], years[1]+1)
+        else: 
+            required_years = [years]
+        print(required_years)
+        for year in required_years:
+            if not (year in self.stack.coords['time']):
+                return False
+        return True
 
     def clip(self):
         pass
+        
     
     def mask(self):
         pass 
