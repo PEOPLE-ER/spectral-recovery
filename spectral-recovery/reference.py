69fc2ab06f00f1da63dcc2315af500a04e1e4039
szwiep <sarahzwiep@gmail.com>
2023-06-12 13:08:09 -0700

    Change to composition for restoration/reference relationship

--- spectral-recovery/reference.py
+++ spectral-recovery/reference.py
@@ -14,34 +14,41 @@ class ReferenceSystem():
     """
     def __init__(
             self,
-            restoration_polygons: gpd.GeoDataFrame,
+            reference_polygons: gpd.GeoDataFrame,
             reference_range: Union[int, List[int]],
-            baseline_method: Callable=None ) -> None:
+            baseline_method: Callable=None
+            ) -> None:
         
-        self.polygons = restoration_polygons
+        self.reference_polygons = reference_polygons
         self.reference_range = reference_range
         self.baseline_method = baseline_method or historic_average
     
     # TODO: some method related to getting bounding boxes
 
-# NOTE: does this sub-class really need to exist?
-class RestorationArea(ReferenceSystem):
-    """ A sub-class of ReferenceSystem. Defines a single area that has
-    been subject to a restoration/disturbance event.
+class RestorationArea():
+    """ Encapsulates data and methods related to reference areas. 
+    Allows optional composition of ReferenceSystem class.
 
     Attributes
     -----------
 
     """
-    def __init__(self, restoration_year: int, *args, **kwargs) -> None:
-        self.restoration_year = restoration_year
-        if self._single_restoration_area(kwargs['restoration_polygons']):
-            super().__init__(*args, **kwargs)
-        else:
+    def __init__(
+            self,
+            restoration_polygon: gpd.GeoDataFrame, 
+            restoration_year: int,
+            reference_system: Union[int, List[int], ReferenceSystem],
+            ) -> None:
+        
+        if not self._single_restoration_area(restoration_polygon):
             return ValueError(
                 f"restoration_polygons contains more than one Polygon."
                 f"A RestorationArea instance can only contain one Polygon." )
-    
+        
+        self.restoration_polygon = restoration_polygon
+        self.restoration_year = restoration_year
+        self.reference_system = reference_system
+            
     def _single_restoration_area(self, restoration_polygons):
         if restoration_polygons.shape[0] != 1:
             return False
