Fix incorrect Y2R computation

spectral-recovery/metrics.py
@@ -1,7 +1,6 @@
 import xarray as xr
 import numpy as np
 
-from typing import Callable
 from enum import Enum
 from utils import maintain_spatial_attrs
 from scipy import stats
@@ -23,9 +22,6 @@ def percent_recovered(
     ) -> xr.DataArray:
     """ Per-pixel percent recovery
 
-    Recovery computation based on a baseline measurement, a restoration 
-    event measure, and the current/relevant observations.
-
     """
     total_change = abs(baseline-event)
     recovered = abs(stack-baseline)
@@ -35,33 +31,61 @@ def percent_recovered(
 @maintain_spatial_attrs
 def years_to_recovery(
     stack: xr.DataArray,
-    baseline: xr.DataArray
+    baseline: xr.DataArray,
+    percent: int = 80,
+    curr_year = int
 ) -> xr.DataArray:
-    reco_80 = baseline * 0.80
-    # print(baseline.data.compute())
-    ts = theil_sen(stack.chunk(dict(time=-1)), stack.time.dt.year)
-    # print(ts.data.compute())
-    temp = reco_80 - stack / ts[0][0][0][0]
-    return temp[0,:,0,0] # TODO: this is insane. Fix the dims before computing.
+    """ Years-to-recovery
+
+    
+    """
+    reco_80 = baseline * (percent / 100)
+    print(baseline.data.compute())
+    # theil_sen calls apply_ufunc along the time dimension so stack's 
+    # chunks need to contain the entire timestack before being passed
+    ts = theil_sen(y=stack.chunk(dict(time=-1)), x=stack.time.dt.year)
+    y2r = ((reco_80 - ts.sel(parameter="intercept")) 
+           / ts.sel(parameter="slope"))
+    return y2r - curr_year
 
 
-def new_linregress(x, y):
-    # Wrapper around scipy mstats.theilslopes to use in apply_ufunc
-    print(f"ts input: {x} and {y}")
-    slope, intercept, low_slope, high_slope = stats.mstats.theilslopes(x, y)
-    return np.array([slope, intercept, low_slope, high_slope])
+def new_linregress(y, x):
+    """ Wrapper around  mstats.theilslopes for apply_ufunc usage """
+    slope, intercept, low_slope, high_slope = stats.mstats.theilslopes(y, x)
+    print(slope, intercept)
+    return np.array([slope, intercept])
 
 
-def theil_sen(spectral, time):
+def theil_sen(y, x):
+    """ Apply theil_sen slope regression across time on each pixel
+    
+    Parameters
+    ----------
+    y : xr.DataArray
 
-    return xr.apply_ufunc(
+    x : list of int
+    
+    Returns
+    -------
+    ts_reg : xr.DataArray
+        DataArray of  theil-sen slope and intercept parameters for each
+        pixel. 3D DataArray with "parameter", "y" and "x" labelled 
+        dimensions where "y" and "x" match input "y" and "x".
+
+    """
+    ts_dim_name = "parameter"
+    ts_reg = xr.apply_ufunc(
         new_linregress,
-        spectral,
-        time,
+        y,
+        x,
         input_core_dims=[['time'], ['time']],
-        output_core_dims=[["parameter"]],
+        output_core_dims=[[ts_dim_name]],
         vectorize=True,
         dask="parallelized",
         output_dtypes=['float64'],
-        dask_gufunc_kwargs={"output_sizes": {"parameter": 4}},
+        dask_gufunc_kwargs={
+            "output_sizes": {ts_dim_name: 2}
+            },
         )
+    ts_reg = ts_reg.assign_coords({"parameter": ["slope", "intercept"]})
+    return ts_reg

spectral-recovery/spectral_recovery.py
@@ -38,11 +38,18 @@ def spectral_recovery(
             Year of restoration event.
 
         """
+        # indices = MultiBandYearlyStack(
+        #      bands=band_dict,
+        #      dict=True,
+        #      data_mask=data_mask
+        #      ).indices(indices_list) # NOTE: indices computed on full stack. Might come to regret this.
+
         indices = MultiBandYearlyStack(
              bands=band_dict,
              dict=True,
              data_mask=data_mask
-             ).indices(indices_list) # NOTE: indices computed on full stack. Might come to regret this.
+             )
+
         ra = RestorationArea(
              restoration_polygon=restoration_poly,
              restoration_year=restoration_year,
@@ -50,9 +57,9 @@ def spectral_recovery(
              composite_stack=indices
         )
         metrics = ra.metrics(metrics_list)
-        a = metrics.sel(metric='years_to_recovery').data.compute()
-        print(a)
-        # data = metrics.sel(metric="years_to_recovery").data.compute()[0][0][0]
+        data = metrics.data.compute()
+        # data = metrics.sel(metric="years_to_recovery").data.compute()
+        print(data)
         # vals = ra.stack.sel(time=slice(ra.restoration_year,ra.end_year)).data.compute()
         # intercept =  ra.stack.sel(time=slice(ra.restoration_year)).squeeze().data.compute()
         # y_vals = intercept[0] + data[0]*vals
@@ -68,37 +75,26 @@ def spectral_recovery(
 
 
 if __name__ == "__main__":
-    test_poly = gpd.read_file("../../data/smaller_poly.gpkg")
-    bad_poly = gpd.read_file("../../data/out_of_bounds_poly.gpkg")
-    rest_year = pd.to_datetime("2013")
-    reference_year = (pd.to_datetime("2010"), pd.to_datetime("2012"))
+    test_poly = gpd.read_file("../../data/1p_test.gpkg")
+    # bad_poly = gpd.read_file("../../data/out_of_bounds_poly.gpkg")
+    rest_year = pd.to_datetime("2012")
+    reference_year = (pd.to_datetime("2009"), pd.to_datetime("2011"))
 
-    test_stack = rioxarray.open_rasterio("../../data/nir_18_19.tif",
+    test_stack = rioxarray.open_rasterio("../../data/test.tif",
                                              chunks="auto")
-    test_stack = xr.ones_like(test_stack)
+    
     test_stack = test_stack.rename({"band":"time"})
-    
-    test_stack =  xr.concat(
-            [test_stack*100, test_stack*100,test_stack*101, test_stack*100, test_stack*11, test_stack,test_stack*2, test_stack*2.5, test_stack*3, test_stack*3.5, test_stack*4, test_stack*4.5], 
-            dim=pd.Index(["2008","2009","2010", "2011","2012","2013","2014", "2015","2016","2017","2018", "2019"], name="time")
-            )
-    # test_mask = xr.where(test_stack > 15515, True, False)
-    test_stack = test_stack.assign_coords(time=(pd.to_datetime(["2008","2009","2010", "2011","2012","2013","2014", "2015","2016","2017","2018", "2019"])))
-    # test_stack.rio.write_crs(test_raster.rio.crs, inplace=True)
-    # test_stack.rio.update_encoding(test_stack.encoding, inplace=True)
+    # # test_mask = xr.where(test_stack > 15515, True, False)
+    test_stack = test_stack.assign_coords(
+          time=(
+                pd.to_datetime(["2008","2009","2010", "2011","2012","2013","2014", "2015","2016","2017","2018", "2019"])))
 
-    # print(test_stack.rio.crs, test_stack.encoding, test_stack.attrs)
-
-    test_band_dict = {"red": test_stack * np.random.randint(low=1, high=50, size=test_stack.shape), 
-                      "nir": test_stack * np.random.randint(low=1, high=50, size=test_stack.shape),
-                      "swir": test_stack * np.random.randint(low=1, high=50, size=test_stack.shape)
-                      }
-    
+    test_band_dict = {"NDVI": test_stack}
     spectral_recovery(
          band_dict=test_band_dict,
          restoration_poly=test_poly,
          restoration_year=rest_year,
          reference_years=reference_year,
          indices_list=["ndvi"],
-         metrics_list=["years_to_recovery"],
+         metrics_list=["percent_recovered", "years_to_recovery"],
          )