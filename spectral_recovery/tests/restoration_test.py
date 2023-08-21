4c9e0504dc16db76b9acfef8b051384446cbaeef
szwiep <szwiep@sfu.ca>
2023-08-21 11:59:13 -0700

    Test restoration and reference system

--- spectral_recovery/tests/restoration_test.py
+++ spectral_recovery/tests/restoration_test.py
@@ -1,4 +1,5 @@
 import pytest
+from spectral_recovery.baselines import historic_average
 import xarray as xr
 import numpy as np
 import rioxarray
@@ -6,127 +7,55 @@ import geopandas as gpd
 import pandas as pd
 
 from mock import patch
+from shapely.geometry import Polygon
+from geopandas.testing import assert_geodataframe_equal
 from spectral_recovery.restoration import ReferenceSystem, RestorationArea
 from spectral_recovery.enums import Metric
 
 
 DATETIME_FREQ = "YS"  # TODO: should this be kept somewhere else in the project? Seem wrong that it's defined again here and in timeseries
 
-
-# This might need some set-up and tear down
-# https://stackoverflow.com/questions/26405380/how-do-i-correctly-setup-and-teardown-for-my-pytest-class-with-tests
-# TODO: test that RA contains the clipped data for a polygon, not the entire AOI
-@pytest.mark.parametrize(
-    ("resto_poly", "resto_year", "ref_sys", "raster", "end_year", "time_range"),
-    [
-        (
-            "1p_test.gpkg",
-            pd.to_datetime("2011"),
-            pd.to_datetime("2010"),
-            "test_recovered.tif",
-            None,
-            [str(x) for x in np.arange(2010, 2022)],
-        ),
-        (
-            "1p_test.gpkg",
-            pd.to_datetime("2014"),
-            (pd.to_datetime("2010"), pd.to_datetime("2013")),
-            "test_recovered.tif",
-            None,
-            [str(x) for x in np.arange(2010, 2022)],
-        ),
-    ],
-)
-def test_RestorationArea_initialization(
-    resto_poly,
-    resto_year,
-    ref_sys,
-    raster,
-    end_year,
-    time_range,
-):
-    with rioxarray.open_rasterio(raster, chunks="auto") as data:
-        stack = data
-        stack = stack.rename({"band": "time"})
-        stack = stack.assign_coords(
-            time=(pd.date_range(time_range[0], time_range[-1], freq=DATETIME_FREQ))
-        )
-        resto_poly = gpd.read_file("1p_test.gpkg")
-        resto_a = RestorationArea(
-            restoration_polygon=resto_poly,
-            restoration_year=resto_year,
-            reference_system=ref_sys,
-            composite_stack=stack,
-            end_year=end_year,
-        )
-        assert isinstance(resto_a.restoration_year, pd.Timestamp)
-        assert (
-            resto_a.restoration_polygon.geometry.geom_equals(resto_poly.geometry)
-        ).all()
-        assert isinstance(resto_a.reference_system, ReferenceSystem)
-        assert resto_a.reference_system.reference_polygons.geom_equals(
-            resto_poly.geometry
-        ).all()
-        assert resto_a.reference_system.reference_range == ref_sys
-
-
-# check fro bad resto year, bad reference year, bad spatial location
-@pytest.mark.parametrize(
-    ("resto_poly", "resto_year", "ref_sys", "raster", "end_year", "time_range"),
-    [
-        (
-            "1p_test.gpkg",
-            pd.to_datetime("2023"),
-            pd.to_datetime("2012"),
-            "test_recovered.tif",
-            None,
-            [str(x) for x in np.arange(2010, 2022)],
-        ),
-        (
-            "1p_test.gpkg",
-            pd.to_datetime("2010"),
-            pd.to_datetime("2009"),
-            "test_recovered.tif",
-            None,
-            [str(x) for x in np.arange(2010, 2022)],
-        ),
-        (
-            "no_overlap.gpkg",
-            pd.to_datetime("2015"),
-            pd.to_datetime("2012"),
-            "test_recovered.tif",
-            None,
-            [str(x) for x in np.arange(2010, 2022)],
-        ),
-        (
-            "not_fully_contained.gpkg",
-            pd.to_datetime("2015"),
-            pd.to_datetime("2012"),
-            "test_recovered.tif",
-            None,
-            [str(x) for x in np.arange(2010, 2022)],
-        ),
-    ],
-)
-def test_RestorationArea_not_contained_error(
-    resto_poly,
-    resto_year,
-    ref_sys,
-    raster,
-    end_year,
-    time_range,
-):
-    with rioxarray.open_rasterio(raster, chunks="auto") as data:
-        stack = data
-        stack = stack.rename({"band": "time"})
-        stack = stack.assign_coords(
-            time=(pd.date_range(time_range[0], time_range[-1], freq=DATETIME_FREQ))
-        )
-        resto_poly = gpd.read_file(resto_poly)
-        with pytest.raises(
-            ValueError,
-            match="RestorationArea not contained by stack. Better message soon!",
-        ):
+class TestRestorationAreaInit:
+    # This might need some set-up and tear down
+    # https://stackoverflow.com/questions/26405380/how-do-i-correctly-setup-and-teardown-for-my-pytest-class-with-tests
+    # TODO: test that RA contains the clipped data for a polygon, not the entire AOI
+    @pytest.mark.parametrize(
+        ("resto_poly", "resto_year", "ref_sys", "raster", "end_year", "time_range"),
+        [
+            (
+                "1p_test.gpkg",
+                pd.to_datetime("2011"),
+                pd.to_datetime("2010"),
+                "test_recovered.tif",
+                None,
+                [str(x) for x in np.arange(2010, 2022)],
+            ),
+            (
+                "1p_test.gpkg",
+                pd.to_datetime("2014"),
+                (pd.to_datetime("2010"), pd.to_datetime("2013")),
+                "test_recovered.tif",
+                None,
+                [str(x) for x in np.arange(2010, 2022)],
+            ),
+        ],
+    )
+    def test_good_init(
+        self,
+        resto_poly,
+        resto_year,
+        ref_sys,
+        raster,
+        end_year,
+        time_range,
+    ):
+        with rioxarray.open_rasterio(raster, chunks="auto") as data:
+            stack = data
+            stack = stack.rename({"band": "time"})
+            stack = stack.assign_coords(
+                time=(pd.date_range(time_range[0], time_range[-1], freq=DATETIME_FREQ))
+            )
+            resto_poly = gpd.read_file("1p_test.gpkg")
             resto_a = RestorationArea(
                 restoration_polygon=resto_poly,
                 restoration_year=resto_year,
@@ -134,9 +63,86 @@ def test_RestorationArea_not_contained_error(
                 composite_stack=stack,
                 end_year=end_year,
             )
+            assert isinstance(resto_a.restoration_year, pd.Timestamp)
+            assert (
+                resto_a.restoration_polygon.geometry.geom_equals(resto_poly.geometry)
+            ).all()
+            assert isinstance(resto_a.reference_system, ReferenceSystem)
+            assert resto_a.reference_system.reference_polygons.geom_equals(
+                resto_poly.geometry
+            ).all()
+            assert resto_a.reference_system.reference_range == ref_sys
 
 
-class TestMetrics:
+    # check fro bad resto year, bad reference year, bad spatial location
+    @pytest.mark.parametrize(
+        ("resto_poly", "resto_year", "ref_sys", "raster", "end_year", "time_range"),
+        [
+            (
+                "1p_test.gpkg",
+                pd.to_datetime("2023"),
+                pd.to_datetime("2012"),
+                "test_recovered.tif",
+                None,
+                [str(x) for x in np.arange(2010, 2022)],
+            ),
+            (
+                "1p_test.gpkg",
+                pd.to_datetime("2010"),
+                pd.to_datetime("2009"),
+                "test_recovered.tif",
+                None,
+                [str(x) for x in np.arange(2010, 2022)],
+            ),
+            (
+                "no_overlap.gpkg",
+                pd.to_datetime("2015"),
+                pd.to_datetime("2012"),
+                "test_recovered.tif",
+                None,
+                [str(x) for x in np.arange(2010, 2022)],
+            ),
+            (
+                "not_fully_contained.gpkg",
+                pd.to_datetime("2015"),
+                pd.to_datetime("2012"),
+                "test_recovered.tif",
+                None,
+                [str(x) for x in np.arange(2010, 2022)],
+            ),
+        ],
+    )
+    def test_not_contained_error(
+        self,
+        resto_poly,
+        resto_year,
+        ref_sys,
+        raster,
+        end_year,
+        time_range,
+    ):
+        with rioxarray.open_rasterio(raster, chunks="auto") as data:
+            stack = data
+            stack = stack.rename({"band": "time"})
+            stack = stack.assign_coords(
+                time=(pd.date_range(time_range[0], time_range[-1], freq=DATETIME_FREQ))
+            )
+            resto_poly = gpd.read_file(resto_poly)
+            with pytest.raises(
+                ValueError,
+                match="RestorationArea not contained by stack. Better message soon!",
+            ):
+                resto_a = RestorationArea(
+                    restoration_polygon=resto_poly,
+                    restoration_year=resto_year,
+                    reference_system=ref_sys,
+                    composite_stack=stack,
+                    end_year=end_year,
+                )
+
+
+class TestRestorationAreaMetrics:
+
     @pytest.fixture()
     def valid_resto_area(self):
         polygon = "1p_test.gpkg"
@@ -184,7 +190,7 @@ class TestMetrics:
         ],
     )
     @patch("spectral_recovery.restoration._stack_bands")
-    def test_RestorationArea_metric_switch_case(
+    def test_switch_case(
         self, mock_stack, valid_resto_area, metric_list, metric_method, mocker
     ):
         metric_patch = mocker.patch(f"spectral_recovery.restoration.{metric_method}")
@@ -195,7 +201,7 @@ class TestMetrics:
         assert mock_stack.call_count == 1
 
     @patch("spectral_recovery.restoration._stack_bands")
-    def test_RestorationArea_multiple_metrics(
+    def test_multiple_metrics(
         self, mock_stack, valid_resto_area, mocker
     ):
         metrics_list = [
@@ -231,7 +237,7 @@ class TestMetrics:
         "spectral_recovery.restoration.dNBR",
         return_value=xr.DataArray([[0.5]], dims=["y", "x"]),
     )
-    def test_RestorationArea_stack_multiple_metrics(
+    def test_stack_multiple_metrics(
         self,
         percent_reco_return,
         dNBR_return,
@@ -250,7 +256,7 @@ class TestMetrics:
         "spectral_recovery.restoration.percent_recovered",
         return_value=xr.DataArray([[1.0]], dims=["y", "x"]),
     )
-    def test_RestorationArea_stack_single_metric(
+    def test_stack_single_metric(
         self,
         percent_reco_return,
         valid_resto_area,
@@ -263,3 +269,93 @@ class TestMetrics:
         )
         result = valid_resto_area.metrics(metric)
         assert result.equals(expected_result)
+
+class TestReferenceSystem:
+
+    @pytest.fixture()
+    def gdf(self):
+        p1 = Polygon([(0, 0), (1, 0), (1, 1)])
+        p2 = Polygon([(0, 0), (1, 0), (1, 1), (0, 1)])
+        g = gpd.GeoSeries([p1, p2])
+        gdf = gpd.GeoDataFrame(geometry=g)
+        return gdf
+    
+    @pytest.fixture()
+    def simple_callable(self):
+        def simple_callable(stack, dates):
+            return 0
+        return simple_callable
+
+    @pytest.fixture()
+    def reference_date(self):
+        return pd.to_datetime("2008")
+
+    def test_init(self, gdf, reference_date):
+        rs = ReferenceSystem(
+            reference_polygons=gdf,
+            reference_range=reference_date,
+            baseline_method=None,
+            variation_method=None
+        )
+        assert_geodataframe_equal(rs.reference_polygons, gdf, check_geom_type=True)
+        assert rs.reference_range == reference_date
+        assert rs.baseline_method == historic_average
+        assert rs.variation_method == None
+
+
+    def test_init_baseline_method(gdf, reference_date, simple_callable):
+        rs = ReferenceSystem(
+            reference_polygons=gdf,
+            reference_range=reference_date,
+            baseline_method=simple_callable,
+            variation_method=None,
+        )
+        assert rs.baseline_method == simple_callable
+        assert rs.variation_method == None
+
+    def test_init_both_variation_and_baseline(gdf, reference_date, simple_callable):
+        rs = ReferenceSystem(
+            reference_polygons=gdf,
+            reference_range=reference_date,
+            baseline_method=simple_callable,
+            variation_method=simple_callable,
+        )
+        assert rs.baseline_method == simple_callable
+        assert rs.variation_method == simple_callable
+
+    @pytest.fixture()
+    def test_stack(self):
+        test_stack = xr.DataArray(np.ones((3, 3, 10, 10))*0, dims=["time","band","y","x"])
+        return test_stack
+
+    def test_no_variation(self, gdf, reference_date, test_stack, simple_callable):
+        expected = {"baseline": 0}
+        rs = ReferenceSystem(
+            reference_polygons=gdf,
+            reference_range=reference_date,
+            baseline_method=None,
+            variation_method=None,
+        )
+        # TODO: this line could be replace with a monkeypatch?
+        rs.baseline_method = simple_callable
+        out = rs.baseline(test_stack)
+        assert out == expected
+
+    def test_with_variation(self, gdf, reference_date, test_stack, simple_callable):
+        expected = {"baseline": 0, "variation": 0}
+        rs = ReferenceSystem(
+            reference_polygons=gdf,
+            reference_range=reference_date,
+            baseline_method=None,
+            variation_method=None,
+        )
+        # TODO: this line could be replace with a monkeypatch?
+        rs.baseline_method = simple_callable
+        rs.variation_method = simple_callable
+        out = rs.baseline(test_stack)
+        assert out == expected
+
+    # @patch("spectral_recovery.restoration.variation_method")
+    # def test_with_variation(self, mock_variation_method):
+    #     pass
+
