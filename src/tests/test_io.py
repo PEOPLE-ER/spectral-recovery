75c70f58a3c598090281585e6832a3d92a3341a3
szwiep <szwiep@sfu.ca>
2023-09-15 16:33:28 -0700

    Update tests

--- src/tests/test_io.py
+++ src/tests/test_io.py
@@ -13,7 +13,6 @@ from spectral_recovery.io.raster import (
 
 
 class TestReadAndStackTifs:
-
     @pytest.mark.parametrize(
         ("tif_paths", "rasterio_return"),
         [
@@ -26,7 +25,11 @@ class TestReadAndStackTifs:
                 ),
             ),
             (
-                [f"a/fake/path/2020.tif", f"a/fake/path/2021.tif", f"a/fake/path/2022.tif"],
+                [
+                    f"a/fake/path/2020.tif",
+                    f"a/fake/path/2021.tif",
+                    f"a/fake/path/2022.tif",
+                ],
                 xr.DataArray(
                     [[[[0]]]],
                     dims=["band", "time", "y", "x"],
@@ -34,7 +37,11 @@ class TestReadAndStackTifs:
                 ),
             ),
             (
-                [f"a/fake/path/2020.tif", f"a/fake/path/2021.tif", f"a/fake/path/2022.tif"],
+                [
+                    f"a/fake/path/2020.tif",
+                    f"a/fake/path/2021.tif",
+                    f"a/fake/path/2022.tif",
+                ],
                 xr.DataArray(
                     [[[[0]]], [[[0]]], [[[0]]]],
                     dims=["band", "time", "y", "x"],
@@ -42,7 +49,11 @@ class TestReadAndStackTifs:
                 ),
             ),
             (
-                [f"a/fake/path/2020.tif", f"a/fake/path/2021.tif", f"a/fake/path/2022.tif"],
+                [
+                    f"a/fake/path/2020.tif",
+                    f"a/fake/path/2021.tif",
+                    f"a/fake/path/2022.tif",
+                ],
                 xr.DataArray(
                     [[[[0, 0], [0, 0]]]],
                     dims=["band", "time", "y", "x"],
@@ -50,7 +61,11 @@ class TestReadAndStackTifs:
                 ),
             ),
             (
-                [f"a/fake/path/2020.tif", f"a/fake/path/2021.tif", f"a/fake/path/2022.tif"],
+                [
+                    f"a/fake/path/2020.tif",
+                    f"a/fake/path/2021.tif",
+                    f"a/fake/path/2022.tif",
+                ],
                 xr.DataArray(
                     [[[[0, 0], [0, 0]]], [[[0, 0], [0, 0]]], [[[0, 0], [0, 0]]]],
                     dims=["band", "time", "y", "x"],
@@ -63,7 +78,10 @@ class TestReadAndStackTifs:
         "rioxarray.open_rasterio",
     )
     def test_correct_stacked_output_sizes(
-        self, mocked_rasterio_open, tif_paths, rasterio_return, 
+        self,
+        mocked_rasterio_open,
+        tif_paths,
+        rasterio_return,
     ):
         mocked_rasterio_open.return_value = rasterio_return
         stacked_tifs = read_and_stack_tifs(paths_to_tifs=tif_paths)
@@ -117,27 +135,33 @@ class TestReadAndStackTifs:
         "rioxarray.open_rasterio",
     )
     def test_bad_filenames_throws_value_err(
-        self, mocked_rasterio_open, filenames, bad_name_index, rasterio_return, 
+        self,
+        mocked_rasterio_open,
+        filenames,
+        bad_name_index,
+        rasterio_return,
     ):
         mocked_rasterio_open.return_value = rasterio_return
         with pytest.raises(
             ValueError,
-            match=f"TIF filenames must be in format 'YYYY' but recived: '{filenames[bad_name_index]}'",
+            match=(
+                "TIF filenames must be in format 'YYYY' but recived:"
+                f" '{filenames[bad_name_index]}'"
+            ),
         ):
             read_and_stack_tifs(paths_to_tifs=filenames)
 
-    
     @pytest.mark.parametrize(
         ("expected_years", "filenames", "expected_bands", "rasterio_return"),
         [
             (
                 [np.datetime64("2019"), np.datetime64("2020"), np.datetime64("2021")],
-                [f"path/to/2019.tif",f"path/to/2020.tif",f"path/to/2021.tif"],
+                [f"path/to/2019.tif", f"path/to/2020.tif", f"path/to/2021.tif"],
                 [BandCommon.blue, BandCommon.red, BandCommon.nir],
                 xr.DataArray(
-                    [[[[0]]],[[[0]]],[[[0]]]],
+                    [[[[0]]], [[[0]]], [[[0]]]],
                     dims=["band", "time", "y", "x"],
-                    attrs={"long_name": ["blue", "red", "nir"]}
+                    attrs={"long_name": ["blue", "red", "nir"]},
                 ),
             ),
         ],
@@ -146,25 +170,33 @@ class TestReadAndStackTifs:
         "rioxarray.open_rasterio",
     )
     def test_correct_coordinate_values(
-        self, mocked_rasterio_open, expected_years, filenames, expected_bands, rasterio_return, 
+        self,
+        mocked_rasterio_open,
+        expected_years,
+        filenames,
+        expected_bands,
+        rasterio_return,
     ):
-
         mocked_rasterio_open.return_value = rasterio_return
         stacked_tifs = read_and_stack_tifs(paths_to_tifs=filenames)
         assert np.all(stacked_tifs["band"].data == expected_bands)
         assert np.all(stacked_tifs["time"].data == expected_years)
 
-
     @pytest.mark.parametrize(
         ("sorted_years", "filenames", "rasterio_return"),
         [
             (
-                [np.datetime64("1990"), np.datetime64("1992"), np.datetime64("2017"), np.datetime64("2018")],
+                [
+                    np.datetime64("1990"),
+                    np.datetime64("1992"),
+                    np.datetime64("2017"),
+                    np.datetime64("2018"),
+                ],
                 [f"2017.tif", f"2018.tif", f"1992.tif", f"1990.tif"],
                 xr.DataArray(
-                    [[[[0]]],[[[0]]],[[[0]]]],
+                    [[[[0]]], [[[0]]], [[[0]]]],
                     dims=["band", "time", "y", "x"],
-                    attrs={"long_name": ["blue", "red", "nir"]}
+                    attrs={"long_name": ["blue", "red", "nir"]},
                 ),
             ),
         ],
@@ -173,21 +205,21 @@ class TestReadAndStackTifs:
         "rioxarray.open_rasterio",
     )
     def test_sorted_years(
-        self, mocked_rasterio_open, sorted_years, filenames, rasterio_return, 
+        self,
+        mocked_rasterio_open,
+        sorted_years,
+        filenames,
+        rasterio_return,
     ):
         mocked_rasterio_open.return_value = rasterio_return
         stacked_tifs = read_and_stack_tifs(paths_to_tifs=filenames)
         assert np.all(stacked_tifs["time"].data == sorted_years)
-    
-
-        
-
 
     # @patch(
     #     "rioxarray.open_rasterio",
     # )
     # def test_stacked_output_sizes(
-    #     self, mocked_rasterio_open, tif_paths, rasterio_return, 
+    #     self, mocked_rasterio_open, tif_paths, rasterio_return,
     # ):
     #     mocked_rasterio_open.return_value = rasterio_return
     #     stacked_tifs = read_and_stack_tifs(paths_to_tifs=tif_paths)
