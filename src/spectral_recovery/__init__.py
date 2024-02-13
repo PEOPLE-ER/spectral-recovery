407fa98f67a559a36c177828e7e42145f01ec41b
szwiep <sarahzwiep@gmail.com>
2024-02-13 15:01:41 -0800

    Rename common names on import

--- src/spectral_recovery/__init__.py
+++ src/spectral_recovery/__init__.py
@@ -7,3 +7,12 @@ processing indices, and computing recovery metrics.
 from spectral_recovery.io.raster import read_and_stack_tifs
 from spectral_recovery.indices import compute_indices
 from spectral_recovery.restoration import RestorationArea
+
+import spyndex as spx
+
+# make 'green' and 'rededge' common names unambiguous
+# need this here until resolution of: https://github.com/awesome-spectral-indices/awesome-spectral-indices/issues/42
+spx.bands["G1"].common_name = "green1"
+spx.bands["RE1"].common_name = "rededge1"
+spx.bands["RE2"].common_name = "rededge2"
+spx.bands["RE3"].common_name = "rededge3"
