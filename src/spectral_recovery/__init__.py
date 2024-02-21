d170fd2a84ba8cadc56784fcfd8cdc3fe6ea712d
szwiep <sarahzwiep@gmail.com>
2024-02-20 22:09:21 -0800

    ENH: create top-level plotting call

--- src/spectral_recovery/__init__.py
+++ src/spectral_recovery/__init__.py
@@ -7,3 +7,4 @@ processing indices, and computing recovery metrics.
 from spectral_recovery.io.raster import read_timeseries
 from spectral_recovery.io.polygon import read_restoration_polygons, read_reference_polygons
 from spectral_recovery.restoration import compute_metrics
+from spectral_recovery.plotting import plot_spectral_trajectory
