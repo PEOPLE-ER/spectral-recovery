96d760b5240bccc70ef4f9b102271d29b2e44c79
szwiep <sarahzwiep@gmail.com>
2024-04-04 16:32:27 -0700

    API: Move compute_indices out of plotting

--- src/spectral_recovery/plotting.py
+++ src/spectral_recovery/plotting.py
@@ -18,9 +18,7 @@ from spectral_recovery.indices import compute_indices
 def plot_spectral_trajectory(
     timeseries_data: xr.DataArray,
     restoration_polygons: gpd.GeoDataFrame,
-    indices: List[str],
     reference_polygons: gpd.GeoDataFrame = None,
-    index_constants: Dict[str, int] = {},
     recovery_target_method=MedianTarget(scale="polygon"),
     path: str = None,
 ):
@@ -42,14 +40,10 @@ def plot_spectral_trajectory(
         Recovery target method to derive recovery target values
 
     """
-
-    indices_stack = compute_indices(
-        image_stack=timeseries_data, indices=indices, constants=index_constants
-    )
     restoration_area = RestorationArea(
         restoration_polygon=restoration_polygons,
         reference_polygons=reference_polygons,
-        composite_stack=indices_stack,
+        composite_stack=timeseries_data,
         recovery_target_method=recovery_target_method,
     )
     if path:
