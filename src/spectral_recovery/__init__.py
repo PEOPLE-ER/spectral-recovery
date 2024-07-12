ef82e6e07b2edb974aa8b878f5051763f03b7925
szwiep <sarahzwiep@gmail.com>
2024-07-12 15:26:07 -0700

    Make historic and reference avail from import

--- src/spectral_recovery/__init__.py
+++ src/spectral_recovery/__init__.py
@@ -6,7 +6,7 @@ processing indices, and computing recovery metrics.
 
 from spectral_recovery.io.raster import read_timeseries
 from spectral_recovery.io.polygon import read_restoration_polygons
-from spectral_recovery import targets
+from spectral_recovery.targets import historic, reference
 from spectral_recovery.indices import compute_indices
 from spectral_recovery.metrics import compute_metrics
 from spectral_recovery.plotting import plot_spectral_trajectory
