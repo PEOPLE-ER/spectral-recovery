15b168d8456214306a12d439a43658116f3322d1
szwiep <sarahzwiep@gmail.com>
2024-08-19 13:43:04 -0700

    Update utils/config imports

--- src/spectral_recovery/io/raster.py
+++ src/spectral_recovery/io/raster.py
@@ -13,13 +13,13 @@ import pandas as pd
 import numpy as np
 import xarray as xr
 
-from spectral_recovery._utils import common_and_long_to_short
-from spectral_recovery._config import SUPPORTED_INDICES
+from spectral_recovery.utils import common_and_long_to_short
 
-from spectral_recovery._config import (
+from spectral_recovery.config import (
     VALID_YEAR,
     REQ_DIMS,
     STANDARD_BANDS,
+    SUPPORTED_INDICES
 )
 
 COMMON_LONG_SHORT_DICT = common_and_long_to_short(STANDARD_BANDS)
